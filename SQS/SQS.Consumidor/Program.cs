594022be103d3ca079a0018751c5143a5f8fab01
Victor Rodrigues dos Santos <victor.rsantos@outlook.com>
2022-06-26 21:43:26 -0300

    Criando Lambda Coletor

---
+++ SQS/SQS.Consumidor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS.Consumidor
+{
+    internal class Program
+    {
+        // Consumir o serviço de SQS, vamos precisar do SDK Install-Package AWSSDK.SQS -Version 3.5.1.14
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Criando o cliente            
+            var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+
+            // Pegar as mensagens da fila
+            var request = new ReceiveMessageRequest { QueueUrl = "https://sqs.sa-east-1.amazonaws.com/437719538875/teste" };
+
+            while (true)
+            {
+
+                var response = await client.ReceiveMessageAsync(request);
+
+                foreach (var mensagem in response.Messages)
+                {
+                    Console.WriteLine(mensagem.Body);
+                    await client.DeleteMessageAsync("https://sqs.sa-east-1.amazonaws.com/437719538875/teste", mensagem.ReceiptHandle);
+                }
+            }
+
+        }
+    }
+}
---
+++ SQS/SQS.Fornecedor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS.Fornecedor
+{
+    internal class Program
+    {
+        // Vai enviar mensagem para fila
+        static async Task Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+            // Forncedor vai enviar mensagem
+            var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+            var request = new SendMessageRequest
+            { 
+                QueueUrl = "https://sqs.sa-east-1.amazonaws.com/437719538875/teste",
+                MessageBody = "teste 123"
+            };
+
+            await client.SendMessageAsync(request);
+
+        }
+    }
+}
